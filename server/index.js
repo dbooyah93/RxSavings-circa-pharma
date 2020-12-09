5167dae69b8a512429e4af18ab4967904f4b09c2
David Boyea <davidmcruz1125@gmail.com>
2020-12-08 23:00:17 -0600

    require(./functionality.js) added to server/index.js and tested

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express()
 const func = require('./functionality.js')
-const PORT = 3000 // consider implementing an .evn file
+const PORT = process.env.PORT || 3000 // test .env file by running node index.js with relevant .env file
 
 app.use( express.urlencoded() );
 app.use( express.json() );
 
 // end point looks like http://localhost:3000/api/loc/38/-91
 app.get('/api/loc/:latitude/:longitude', ( req, res ) => {
-  res.send( `Latitude ${req.param( 'latitude' )} and Longitude ${req.param( 'longitude' )}`)
+  let result;
+  // implement func.search(lat, long)
+  // result = ^^^^^^^^^^^
+  func.search( req.param( 'latitude' ), req.param( 'longitude' ) )
+  // NEEDS res.send() TO EXECUTE PROPERLY
 })
+
+func.test( );
 
 app.listen( PORT, () => {
   console.log( `Listening on.... ${PORT}` );
