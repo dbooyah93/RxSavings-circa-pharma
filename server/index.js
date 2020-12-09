fcd4db7588bc506bfe4308f550888dbd32b7d836
David Boyea <davidmcruz1125@gmail.com>
2020-12-08 20:55:25 -0600

    Confirms working end point with example comment on line 8

--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ const PORT = 3000 // consider implementing an .evn file
 app.use( express.urlencoded() );
 app.use( express.json() );
 
+// end point looks like http://localhost:3000/api/loc/38/-91
 app.get('/api/loc/:latitude/:longitude', ( req, res ) => {
-  res.send( req.param( 'latitude' ) );
+  res.send( `Latitude ${req.param( 'latitude' )} and Longitude ${req.param( 'longitude' )}`)
 })
 
 app.listen( PORT, () => {
