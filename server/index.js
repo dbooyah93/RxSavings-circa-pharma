00fb5d6d743cc1a1a4252c24a700dc4050ff6f84
David Boyea <davidmcruz1125@gmail.com>
2020-12-08 20:47:09 -0600

    Rough draft of API before testing connection

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+const PORT = 3000 // consider implementing an .evn file
+
+app.use( express.urlencoded() );
+app.use( express.json() );
+
+app.get('/api/loc/:latitude/:longitude', ( req, res ) => {
+  res.send( );
+})
+
+app.listen( PORT, () => {
+  console.log( 'Listening on....' PORT );
+});
