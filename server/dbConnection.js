40f683f7c53c8149c924f71f65370e84073ef801
David Boyea <davidmcruz1125@gmail.com>
2020-12-08 20:46:49 -0600

    Refactor files for clarity / organization

---
+++ server/dbConnection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const session = mysql.createConnection({
+  host: 'localhost',
+  user: 'rxuser',
+  password: 'rxuser',
+  database: 'RxPharmas'
+})
+
+session.connect( ( err ) => {
+  if ( err ) {
+    console.log( 'THERE WAS AN ERROR: ' + err );
+  } else {
+    console.log( 'connected' );
+  }
+})
