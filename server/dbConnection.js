6f197a4c887541660279051e37b33abc84e1670e
David Boyea <davidmcruz1125@gmail.com>
2020-12-09 12:02:43 -0600

    Rough draft of a few chocies for how to connect and query

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,6 +1,7 @@
-const mysql = require('mysql');
+const mysql = require('promise-mysql');
 
-const session = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: 'localhost',
   user: 'rxuser',
   password: 'rxuser',
@@ -8,14 +9,45 @@ const session = mysql.createConnection({
 })
 
 let query = `SELECT * FROM pharmas WHERE latitude BETWEEN 38 AND 39 AND longitude BETWEEN -95 AND -94`;
-console.log( session.query( query, ( err, results ) => {
-  if ( err ) {
-    return err;
-  }
-  return results;
-}));
+console.log( 'pre' );
 
+pool
+  .then( ( conn ) => {
+    return conn.getConnection()
+  })
+  .then( ( conn ) => {
+    return conn.query( query )
+  })
+  .then( ( result ) => {
+    console.log( result );
+  })
+  .catch( ( err ) => {
+    console.log( err)
+  })
+
+// session
+//   .then( ( con ) => {
+//     let result = con.query( query );
+//     con.end();
+//     return result;
+//   })
+//   .then( ( results ) => {
+//     console.log( results );
+//   })
+//   .catch ( ( error ) => {
+//     console.log( 'There was an error ' + error )
+//   })
+console.log( 'post' );
+
+/** Clean code for using session to query the RxPharmas DB **/
+// console.log( session.query( query, ( err, results, fields ) => {
+//   if ( err ) {
+//     return console.log( `there was an error: .... ${err}` );
+//   }
+//   console.log( results );
+// }) );
+// console.log('2')
 
 module.exports = {
-  session
+  pool
 }
