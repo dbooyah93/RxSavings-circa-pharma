6ab005f7a10ae123fa48da6954590df74e4293ce
David Boyea <davidmcruz1125@gmail.com>
2020-12-10 08:54:32 -0600

    Implement enviroment variables

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,11 +1,13 @@
+const dotenv = require('dotenv').config();
 const mysql = require('promise-mysql');
+
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: 'localhost',
-  user: 'rxuser',
-  password: 'rxuser',
-  database: 'RxPharmas'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE
 })
 
 module.exports = {
